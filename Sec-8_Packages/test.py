700162a094a82b59f9c9f56f1e967cac06cb37a5
Pangavhane Aditya Anil <141906122+adityapangavhane18@users.noreply.github.com>
2025-06-30 21:53:20 +0530

    Add files via upload

---
+++ Sec-8_Packages/test.py
@@ -0,0 +1,9 @@
+from package.maths import *
+
+## calling the file inside subpackage
+from package.subpackage.mult import mul
+
+## calling the file inside the packge 
+print(addition(2,4))
+print(subtraction(9,2))
+print(mul(5,4))
